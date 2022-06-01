63fb0a854ec60926c502adbe2ccee0d84e624b32
Ramon Mauricio <ArgoTenacius@gmail.com>
2022-06-01 20:46:11 -0300

    Fixed route issue.

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className='homepage__description'>
         <h1 className='homepage__description-title'> Keyboard Shop </h1>
         <h2 className='homepage__description-phrase'> Where you can find the keyboard of the dreams </h2>
-        <Link to={'/shopPage'}>
+        <Link to={'/shopping-cart/shopPage'}>
           <button className='homepage__description-button'>Get yourself one</button>
         </Link>
       </div>
